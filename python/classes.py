fc1ff2b20ced3562a9241831e6cab61a6ac7aa90
Clare Barton <grimalkinclare@gmail.com>
2019-02-20 10:07:50 -0500

    new classes exercise

---
+++ python/classes.py
@@ -0,0 +1,13 @@
+# We will give our characters these attributes:
+# Name
+# avatar (profile pcitre)
+# inventory
+
+# def do_stuff():
+#     pass
+
+
+class Character():
+    # "dunder init"
+    def __init__(self):
+    pass
