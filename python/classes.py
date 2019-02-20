b90a837762ded5dd6579a80b4984d1a8eca57ef8
Clare Barton <grimalkinclare@gmail.com>
2019-02-20 11:19:15 -0500

    adds avatar attribute

--- python/classes.py
+++ python/classes.py
@@ -9,5 +9,8 @@
 
 class Character():
     # "dunder init"
-    def __init__(self):
-    pass
+    def __init__(self, new_name, new_avatar):
+        # 'self' is the customary was yo refer to the instance being built.
+        # in other languages, same as 'this'
+        self.name = new_name
+        self.avatar = new_avatar
