886e54ebf95bc81ef0b8afb2f38c20e22be16355
Clare Barton <grimalkinclare@gmail.com>
2019-02-20 11:17:56 -0500

    adds a test file

---
+++ python/tests.py
@@ -0,0 +1,11 @@
+# NOT REAL TESTS :P
+
+from classes import Character
+
+# characters can be instatiated with name and avatar
+
+arya = Character("No-one", "maisie.png")
+jon = Character("Jon Snow", "kit.png")
+
+print(arya.name, arya.avatar)
+print(jon.name, jon.avatar)
