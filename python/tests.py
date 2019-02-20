8ed2c32971877c6a407ca982dc15b928fe7c5183
Clare Barton <grimalkinclare@gmail.com>
2019-02-20 11:48:22 -0500

    adds greet method and test

--- python/tests.py
+++ python/tests.py
@@ -9,3 +9,12 @@ jon = Character("Jon Snow", "kit.png")
 
 print(arya.name, arya.avatar)
 print(jon.name, jon.avatar)
+
+# arya's inventory length should be 2
+arya.inventory.append('sword')
+arya.inventory.append('mask')
+print(len(arya.inventory))
+
+# testing for methods
+# arya should have a greet method, when I call it, should return "the girl is noone"
+print(arya.greet())
