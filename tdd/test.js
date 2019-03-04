b8791dfeebdcd072fda4a12fb89556ecca90d969
Clare Barton <grimalkinclare@gmail.com>
2019-03-04 15:54:47 -0500

    starts javascript and testing

--- tdd/test.js
+++
@@ -1,8 +0,0 @@
-//like an import in python
-const assert = require('assert');
-
-describe("Addition", function () {
-    interface("Should equal 2 when we add 1 and 1", function () {
-        assert.equal(2, 1, 1);
-    });
-});
